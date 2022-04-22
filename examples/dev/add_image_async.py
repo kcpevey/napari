854ab3dff5e9f3c3c3bdffd2fe4c6c7232f6cffa
Andy Sweet <andrew.d.sweet@gmail.com>
2022-04-22 10:48:53 -0700

    Update image example

--- examples/dev/add_image_async.py
+++ examples/dev/add_image_async.py
@@ -1,9 +1,11 @@
 # A simple driving example to test async slicing.
 
-from skimage import data
+import numpy as np
 import napari
 
-viewer = napari.view_image(data.brain())
+image_data = np.random.rand(100, 1024, 1024)
+
+viewer = napari.view_image(image_data)
 
 if __name__ == '__main__':
     napari.run()
