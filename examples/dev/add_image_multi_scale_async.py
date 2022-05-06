07e709daa7f159a2c8a408ce2502201eedfea5a3
Andy Sweet <andrew.d.sweet@gmail.com>
2022-05-06 14:14:43 -0700

    Fix multi scale example

--- examples/dev/add_image_multi_scale_async.py
+++ examples/dev/add_image_multi_scale_async.py
@@ -2,12 +2,12 @@
 
 import numpy as np
 import napari
-from skimage.transform import pyramid_gaussian
+from skimage.transform import rescale
 
-hires_data = np.random.rand(1000, 256, 256)
-multiscale = list(
-    pyramid_gaussian(hires_data, downscale=2, max_layer=4, multichannel=True)
-)
+hires_data = np.random.rand(100, 1024, 1024)
+midres_data = rescale(hires_data, 0.5, channel_axis=[0])
+lowres_data = rescale(midres_data, 0.5, channel_axis=[0])
+multiscale = [hires_data, midres_data, lowres_data]
 
 viewer = napari.view_image(multiscale, multiscale=True)
 
