417eccdd682dbd1b53df3ab8a87a03d9c30e0815
Kim Pevey <kcpevey@gmail.com>
2022-08-05 08:59:31 -0500

    switch to response, no longer hit on_data_change

--- napari/_vispy/layers/vectors.py
+++ napari/_vispy/layers/vectors.py
@@ -1,7 +1,12 @@
+import logging
+
 import numpy as np
 
 from ..visuals.vectors import VectorsVisual
 from .base import VispyBaseLayer
+from napari.layers.base.base import LayerSliceResponse
+
+LOGGER = logging.getLogger("napari._vispy.layers.vectors")
 
 
 class VispyVectorsLayer(VispyBaseLayer):
@@ -12,33 +17,28 @@ class VispyVectorsLayer(VispyBaseLayer):
         self.layer.events.edge_color.connect(self._on_data_change)
 
         self.reset()
-        self._on_data_change()
+        # self._on_data_change()
 
-    def _on_data_change(self):
-        if (
-            len(self.layer._view_vertices) == 0
-            or len(self.layer._view_faces) == 0
-        ):
-            vertices = np.zeros((3, self.layer._ndisplay))
-            faces = np.array([[0, 1, 2]])
-            face_color = np.array([[0, 0, 0, 0]])
-        else:
-            vertices = self.layer._view_vertices[:, ::-1]
-            faces = self.layer._view_faces
-            face_color = self.layer._view_face_color
-
-        if self.layer._ndisplay == 3 and self.layer.ndim == 2:
-            vertices = np.pad(vertices, ((0, 0), (0, 1)), mode='constant')
+    def _set_slice(self, response: LayerSliceResponse) -> None:
+        """This method replaces the old on_data_change
+        
+        Response data needs face_color
+        """
+        LOGGER.debug('VispyVectorsLayer._set_slice : %s', response.request)
 
         # self.node.set_data(
         #     vertices=vertices, faces=faces, color=self.layer.current_edge_color
         # )
         self.node.set_data(
-            vertices=vertices,
-            faces=faces,
-            face_colors=face_color,
+            vertices=response.data.vertices,
+            faces=response.data.faces,
+            face_colors=response.data.face_color,
         )
 
         self.node.update()
         # Call to update order of translation values with new dims:
         self._on_matrix_change()
+
+    def _on_data_change(self):
+        """this is replaced by set_slice, all the layer data gets moved out"""
+        raise Exception("Deprecated method; should not be called")
