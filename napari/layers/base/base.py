2d0afe348a8eb4d20082265f6f3867d0dd2bc602
Andy Sweet <andrew.d.sweet@gmail.com>
2023-04-03 17:16:52 -0700

    Deprecate loaded

--- napari/layers/base/base.py
+++ napari/layers/base/base.py
@@ -531,7 +531,13 @@ class Layer(KeymapProvider, MousemapProvider, ABC):
         This base class says that layers are permanently in the loaded state.
         Derived classes that do asynchronous loading can override this.
         """
-        # TODO: deprecate?
+        warnings.warn(
+            trans._(
+                'Layer.loaded is deprecated from napari version 0.5 and will be removed in a later version.'
+            ),
+            DeprecationWarning,
+            stacklevel=1,
+        )
         return True
 
     @property
